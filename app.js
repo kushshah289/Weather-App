d65bb1b668df19bfc8a3c2c1d8c2354167199d62
kush <ks4437@nyu.edu>
2018-02-15 10:26:00 -0500

    yargs added

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const request = require('request');
+const yargs = require('yargs');
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: "http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20%20street%20philadelphia",
+  url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error , response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  if(error){
+    console.log('Unable to connect to Google Servers.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find the address.');
+  } else if (body.status === 'OK'){
+    console.log(`Address ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
 });
