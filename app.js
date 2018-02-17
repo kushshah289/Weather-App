d4c6dc72d62fb58647c28da8dcddd9d97313ecb2
kush <ks4437@nyu.edu>
2018-02-17 18:55:48 -0500

    forest.io api added

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
+// const yargs = require('yargs');
+// const geocode = require('./geocode/geocode');
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if(errorMessage){
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+
+
 const request = require('request');
-const yargs = require('yargs');
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
-
-var encodedAddress = encodeURIComponent(argv.address);
-
 request({
-  url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+  url: 'https://api.forecast.io/forecast/c40c98f8a7983478de182873b20a6596/37.8267,-122.4233',
   json: true
-}, (error , response, body) => {
+}, (error, response, body) => {
   if(error){
-    console.log('Unable to connect to Google Servers.');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find the address.');
-  } else if (body.status === 'OK'){
-    console.log(`Address ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    console.log('Unable to connect to forest.io servers');
+  } else if (response.statusCode === 400) {
+    console.log('Unable ot fetch weather.');
+  } else if (response.statusCode === 200){
+
   }
+  console.log(body.currently.temperature);
 });
---
+++ geocode/geocode.js
@@ -0,0 +1,30 @@
+const request = require('request');
+
+var geocodeAddress = (address, callback) => {
+  var encodedAddress = encodeURIComponent(address);
+
+  request({
+    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true
+  }, (error , response, body) => {
+    if(error){
+      callback('Unable to connect to Google Servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+      callback('Unable to find the address.');
+    } else if (body.status === 'OK'){
+      callback(undefined, {
+        address: body.results[0].formatted_address,
+        latitude: body.results[0].geometry.location.lat,
+        longitude: body.results[0].geometry.location.lng
+      });
+      }
+  });
+}
+
+module.exports = {
+  geocodeAddress
+}
+
+
+
+c40c98f8a7983478de182873b20a6596
